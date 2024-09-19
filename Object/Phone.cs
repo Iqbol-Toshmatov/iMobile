25dab8ee23eb2c5c3587d1a4b842d335ce85d346
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 11:40:40 +0500

    Added class Phone.
    This class describes phones which will sale in this application.

---
+++ Object/Phone.cs
@@ -0,0 +1,15 @@
+﻿
+namespace iMobile.Object.Entity
+{
+    public class Phone
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Model { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime? Onsale { get; set; }
+    }
+
+}
