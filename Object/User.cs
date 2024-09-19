4eec9c58f6ef714f8cc3022f3ea8f0de5ff5771d
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:21:14 +0500

    Added file User.cs

---
+++ Object/User.cs
@@ -0,0 +1,18 @@
+﻿using iMobile.Object.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Object.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+        public Role Role { get; set; }
+        public Profile Profile { get; set; }
+    }
+}
