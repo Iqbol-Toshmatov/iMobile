de87b88c67c9595da2d66f5e4f2200c00016e7ef
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:24:55 +0500

    Added AccountController.cs

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using iMobile.Object.ViewModels.Account;
+using iMobile.Service.Implementations;
+using Microsoft.AspNetCore.Mvc;
+using iMobile.Object.Enum;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace iMobile.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AccountService _accountService;
+
+        public AccountController(AccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public IActionResult Register()=>View();
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _accountService.Register(model);
+                if (response.StatusCode==Object.Enum.StatusCode.Ok)
+                {
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new System.Security.Claims.ClaimsPrincipal(response.Data));
+
+                    return RedirectToAction("Index","Home");
+                }
+
+                ModelState.AddModelError("", response.Description);
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+        
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _accountService.Login(model);
+                if (response.StatusCode == Object.Enum.StatusCode.Ok)
+                {
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                        new System.Security.Claims.ClaimsPrincipal(response.Data));
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", response.Description);
+            }
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index","Home")
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
