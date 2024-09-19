92cc3bd44a9af060d0f0b79124898c1ba310d0bc
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:32:18 +0500

    Added profile.
    This class stores data about users.

---
+++ Object/Profile.cs
@@ -0,0 +1,18 @@
+﻿using iMobile.Object.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Object.Entity
+{
+    public class Profile
+    {
+        public long Id { get; set; }
+        public string Address { get; set; } 
+        public short Age { get; set; }
+        public User User { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
