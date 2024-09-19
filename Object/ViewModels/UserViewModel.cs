e37e74c991f55c803c0a38502acc6c6f365ee3a1
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:27:03 +0500

    Added UserViewModel.cs

---
+++ Object/ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Object.ViewModels.User
+{
+    public class UserViewModel
+    {
+        [Display(Name ="Id")]
+        public long Id { get; set; }
+
+        [Required(ErrorMessage ="Enter role")]
+        [Display(Name ="Role")]
+        public string Role { get; set; }
+
+        [Required(ErrorMessage = "Enter login")]
+        [Display(Name = "Login")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+    }
+}
