b7e6ee8b64a4772682dc725757aad7cf7cc932f6
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 11:50:48 +0500

    Added file StartUp.cs
    This file stores connection string to database and other configurations.

---
+++ StartUp.cs
@@ -0,0 +1,58 @@
+﻿using iMobile.DAL;
+using iMobile.DAL.Interfaces;
+using iMobile.DAL.Repositories;
+using iMobile.Service.Implementations;
+using iMobile.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace iMobile
+{
+    public class StartUp
+    {
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddControllersWithViews();
+
+            var connection = Configuration.GetConnectionString("DafaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connection));
+
+            services.AddScoped<IPhoneRepository, PhoneRepository>();
+            services.AddScoped<IPhoneService, PhoneService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
