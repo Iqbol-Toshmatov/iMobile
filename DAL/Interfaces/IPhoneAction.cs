9a77dfd556b517e2e242ed0a1f1e0d903b2e0218
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 11:58:01 +0500

    Added interface IPhoneAction.
    This interface inherited from IBaseAction and it is specialized for the phone entity.

---
+++ DAL/Interfaces/IPhoneAction.cs
@@ -0,0 +1,11 @@
+﻿using iMobile.Object.Entity;
+
+namespace iMobile.DAL.Interfaces
+{
+    public interface IPhoneRepository:IBaseRepository<Phone>
+    {
+        Task<bool> Create(Phone entity);
+        Task<bool> Delete(Phone entity);
+        Task<Phone> GetByName(string name);
+    }
+}
