2043e5cfde1cbe1e8879ed68453cb37f92f9c77c
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:23:44 +0500

    Update HashPasswordHelper.cs

--- Object/Helpers/HashPasswordHelper.cs
+++ Object/Helpers/HashPasswordHelper.cs
@@ -1,1 +1,21 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Object.Helpers
+{
+    public static class HashPasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256=SHA256.Create())
+            {
+                var hashedBytes=sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var hash=BitConverter.ToString(hashedBytes).Replace("-","").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
 
