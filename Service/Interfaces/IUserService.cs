15d2ec763afcf280875b5ba0f42fe10efdf0ad78
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:42:13 +0500

    Added IUserService.cs

---
+++ Service/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using iMobile.Object.Entity;
+using iMobile.Object.Response;
+using iMobile.Object.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IBaseResponse<User>> Create(UserViewModel user);
+        BaseResponse<Dictionary<int, string>> GetRoles();
+        Task<BaseResponse<IEnumerable<UserViewModel>>> GetUsers();
+        Task<BaseResponse<bool>> DeleteUser(int id);
+    }
+}
