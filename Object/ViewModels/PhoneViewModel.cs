1a6ac12f7bfd94085a36da41136589d24f65989b
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:29:01 +0500

    Added PhoneViewModel.cs

---
+++ Object/ViewModels/PhoneViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Object.ViewModels.Phone
+{
+    public class PhoneViewModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Model { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public DateTime? Onsale { get; set; }
+    }
+}
