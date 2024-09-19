37e1840d4d79640f8764d86bb2af8a06398e8460
Iqbol-Toshmatov <iqboltoshmatov1105@gmail.com>
2024-09-19 12:31:03 +0500

    Added Role.cs

---
+++ Object/Enum/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMobile.Object.Enum
+{
+    public enum Role
+    {
+        Admin=0,
+        User=1,
+        Moderator=2
+    }
+}
